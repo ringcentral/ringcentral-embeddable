b9b90a75345793e2ee8c64235b9ef48ca243193c
embbnux <embbnux@embbnux.com>
2017-12-04 10:46:29 +0800

    remove unused codes

--- src/modules/Interaction/actionTypes.js
+++
@@ -1,6 +0,0 @@
-import Enum from 'ringcentral-integration/lib/Enum';
-import moduleActionTypes from 'ringcentral-integration/enums/moduleActionTypes';
-
-export default new Enum([
-  ...Object.keys(moduleActionTypes),
-]);
--- src/modules/Interaction/getReducer.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux';
-import getModuleStatusReducer from 'ringcentral-integration/lib/getModuleStatusReducer';
-
-export default function getReducer(types) {
-  return combineReducers({
-    status: getModuleStatusReducer(types),
-  });
-}
