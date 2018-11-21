b233e6fb484858e7d8ff2eb63cbe79727be586cf
embbnux <embbnux@embbnux.com>
2018-11-21 10:28:05 +0800

    support to show note from log matcher

--- src/components/LogSectionModal/index.js
+++ src/components/LogSectionModal/index.js
@@ -11,7 +11,6 @@ import styles from './styles.scss';
 export default class LogSectionModal extends Component {
   constructor(props) {
     super(props);
-
     this.state = {
       note: '',
     };
@@ -20,15 +19,24 @@ export default class LogSectionModal extends Component {
   }
 
   componentWillReceiveProps(nextProps) {
+    if (!nextProps.currentCall) {
+      return;
+    }
     if (
-      nextProps.currentCall &&
       nextProps.currentCall.id !== this._lastCallId
     ) {
       this._lastCallId = nextProps.currentCall.id;
+      this.props.onLoadData(nextProps.currentCall);
       this.setState({
         note: '',
       });
-      this.props.onLoadData(nextProps.currentCall);
+    }
+    const nextMatcher = this.getLogMatcher(nextProps);
+    const matcher = this.getLogMatcher(this.props);
+    if (nextMatcher && nextMatcher !== matcher) {
+      this.setState({
+        note: nextMatcher.note || '',
+      });
     }
   }
 
@@ -44,6 +52,16 @@ export default class LogSectionModal extends Component {
       call: this.props.currentCall,
       note: this.state.note,
     });
+  }
+
+  getLogMatcher(props = this.props) {
+    if (!props.currentCall) {
+      return null;
+    }
+    if (props.currentCall.activityMatches && props.currentCall.activityMatches.length) {
+      return props.currentCall.activityMatches[0];
+    }
+    return null;
   }
 
   renderEditSection = () => {
