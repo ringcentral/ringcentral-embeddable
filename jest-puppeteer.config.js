82cb090870d9f41ea5ccc798e7e7ebe5b36d4cd2
embbnux <embbnux@embbnux.com>
2018-12-14 17:09:35 +0800

    upgrade packages

--- jest-puppeteer.config.js
+++ jest-puppeteer.config.js
@@ -1,9 +1,9 @@
 module.exports = {
-  // server: {
-  //   command: 'yarn start',
-  //   port: 8080,
-  //   launchTimeout: 100000
-  // },
+  server: {
+    command: 'yarn start',
+    port: 8080,
+    launchTimeout: 100000
+  },
   launch: {
     dumpio: true,
     headless: process.env.HEADLESS === 'true'
