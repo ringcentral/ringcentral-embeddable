0598b01bce95d46c79b227f8e8d51a060b63fe97
embbnux <embbnux@embbnux.com>
2019-01-07 15:40:15 +0800

    upgrade webpack devserver and increase timeout

--- src/lib/requestWithPostMessage.js
+++ src/lib/requestWithPostMessage.js
@@ -1,13 +1,13 @@
 import uuid from 'uuid';
 
-export default function requestWithPostMessage(path, body) {
+export default function requestWithPostMessage(path, body, timeout = 3000) {
   return new Promise((resolve, reject) => {
     const id = uuid.v4();
     let responseFunc;
     const catchTimeout = setTimeout(() => {
       window.removeEventListener('message', responseFunc);
       reject('Time out');
-    }, 3000);
+    }, timeout);
     responseFunc = (e) => {
       const data = e.data;
       if (
