3af49b43c0e4ddacf1a9e99aba59b1ec374642d2
embbnux <embbnux@embbnux.com>
2019-03-19 11:17:54 +0800

    update puppeteer config

--- jest-puppeteer.config.js
+++ jest-puppeteer.config.js
@@ -1,13 +1,15 @@
 module.exports = {
   server: {
-    command: 'NODE_ENV=development yarn start', // use developement babel setting
+    command: 'NODE_ENV=development yarn start', // use development babel setting
     port: 8080,
     launchTimeout: 100000
   },
   launch: {
     dumpio: true,
+    args: ['--use-fake-ui-for-media-stream', '--use-fake-device-for-media-stream'],
     headless: process.env.TEST_HEADLESS === 'true'
     // TODO: widget will check visible before init app,
     // so in headless mode, widget will keep loadding.
   },
+  browserContext: 'incognito',
 };
