086c3f90821c6472a9ceb5daa2f5ab8a6c4ee6eb
embbnux <embbnux@embbnux.com>
2019-08-16 01:22:04 +0800

    remove unused

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -172,7 +172,7 @@ export default function App({
               <Route
                 path="/history"
                 component={() => (
-                  <div style={{ width: '100%', height: '100%', position: 'relative' }}>
+                  <div style={{ width: '100%', height: '100%' }}>
                     <CallsListPage />
                     <CallLogSectionModal />
                   </div>
