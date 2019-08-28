0111473e7e60d466f6883c04a595a68151f12b26
embbnux <embbnux@embbnux.com>
2019-08-28 17:28:47 +0800

    update with 0.10.12

--- src/modules/Phone/index.js
+++ src/modules/Phone/index.js
@@ -48,7 +48,7 @@ import Softphone from 'ringcentral-integration/modules/Softphone';
 import Storage from 'ringcentral-integration/modules/Storage';
 import Subscription from 'ringcentral-integration/modules/Subscription';
 import TabManager from 'ringcentral-integration/modules/TabManager';
-// import Webphone from 'ringcentral-integration/modules/Webphone';
+import Webphone from 'ringcentral-integration/modules/Webphone';
 import ContactDetails from 'ringcentral-integration/modules/ContactDetails';
 import Feedback from 'ringcentral-integration/modules/Feedback';
 import Conference from 'ringcentral-integration/modules/Conference';
@@ -99,7 +99,6 @@ import ErrorLogger from '../ErrorLogger';
 import ActiveCalls from '../ActiveCalls';
 import CallingSettings from '../CallingSettings';
 import CallLog from '../CallLog';
-import Webphone from '../Webphone';
 
 import searchContactPhoneNumbers from '../../lib/searchContactPhoneNumbers';
 import hackSend from '../../lib/hackSend';
