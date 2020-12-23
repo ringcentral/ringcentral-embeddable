40ba846234028f8cde8fd794cf320b53a66e69a5
embbnux <embbnux@embbnux.com>
2020-12-23 17:56:54 +0800

    enhance: patch getUserMedia to show alert

--- src/lib/patchGetUserMedia.js
+++ src/lib/patchGetUserMedia.js
@@ -1,32 +1,30 @@
-const isFirefox = navigator.userAgent.indexOf('Firefox') > -1;
-let lastUserMediaStreams = null;
-let lastGetUserMediaTimestamp = null;
-let getUserMediaPromise = null;
-
-if (isFirefox && navigator.mediaDevices.getUserMedia) {
+if (navigator.mediaDevices.getUserMedia) {
   navigator.mediaDevices._$getUserMedia = navigator.mediaDevices.getUserMedia;
   navigator.mediaDevices.getUserMedia = async (constraints) => {
-    if (
-      lastGetUserMediaTimestamp &&
-      Date.now() - lastGetUserMediaTimestamp < 3000 &&
-      lastUserMediaStreams
-    ) {
-      return lastUserMediaStreams;
+    if (!document.hidden) {
+      return navigator.mediaDevices._$getUserMedia(constraints);
     }
-    let streams;
-    try {
-      if (!getUserMediaPromise) {
-        getUserMediaPromise = navigator.mediaDevices._$getUserMedia(constraints);
+    // show alert if not response after 3s
+    let timeout = setTimeout(() => {
+      timeout = null;
+      if (window.phone && window.phone.alert) {
+        window.phone.alert.warning({
+          message: 'allowMicrophonePermissionOnInactiveTab',
+          ttl: 0,
+        });
       }
-      streams = await getUserMediaPromise;
-      lastUserMediaStreams = streams;
-      lastGetUserMediaTimestamp = Date.now();
-      getUserMediaPromise = null;
+    }, 3000);
+    try {
+      const result = await navigator.mediaDevices._$getUserMedia(constraints);
+      if (timeout !== null) {
+        clearTimeout(timeout);
+      }
+      return result;
     } catch (e) {
-      getUserMediaPromise = null;
-      lastUserMediaStreams = null;
+      if (timeout !== null) {
+        clearTimeout(timeout);
+      }
       throw e;
     }
-    return streams;
   };
 }
