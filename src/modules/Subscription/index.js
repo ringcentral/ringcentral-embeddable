84f979b3f2dc514d5ec3be511e8136165055dd5b
Embbnux Ji <embbnux@embbnux.com>
2021-03-25 17:15:36 +0800

    fix: sleep detect issue in subscription (#495)

---
+++ src/modules/Subscription/index.js
@@ -0,0 +1,22 @@
+import Subscription from 'ringcentral-integration/modules/Subscription';
+import { Module } from 'ringcentral-integration/lib/di';
+
+@Module({
+  name: 'NewSubscription',
+  deps: []
+})
+export default class NewSubscription extends Subscription {
+
+  // fix in widgets lib for https://developer.chrome.com/blog/timer-throttling-in-chrome-88/
+  _detectSleep() {
+    const t = Date.now();
+    this._sleepTimeout = setTimeout(async () => {
+      if (this.ready && this._subscription && Date.now() - t > 62 * 1000) {
+        console.log('==== Sleep Detected =====');
+        await this.remove();
+        await this._subscribe();
+      }
+      this._detectSleep();
+    }, 10 * 1000);
+  }
+}
