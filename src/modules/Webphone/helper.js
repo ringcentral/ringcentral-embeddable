417f2cc07ceb0511e2b89ad03abe0950db2ec84c
embbnux <embbnux@embbnux.com>
2021-05-31 16:45:48 +0800

    fix: show call queue name at call

---
+++ src/modules/Webphone/helper.js
@@ -0,0 +1,38 @@
+import {
+  normalizeSession as normalizeSessionBase
+} from 'ringcentral-integration/modules/Webphone/webphoneHelper';
+
+import callDirections from 'ringcentral-integration/enums/callDirections';
+
+function getCallQueueName({ direction, headers }) {
+  if (
+    direction === callDirections.outbound ||
+    !headers ||
+    !headers['P-Rc-Api-Call-Info'] ||
+    !headers['P-Rc-Api-Call-Info'][0] ||
+    !headers['P-Rc-Api-Call-Info'][0].raw ||
+    !headers['P-Asserted-Identity'] ||
+    !headers['P-Asserted-Identity'][0] ||
+    !headers['P-Asserted-Identity'][0].raw
+  ) {
+    return null;
+  }
+  if (headers['P-Rc-Api-Call-Info'][0].raw.indexOf('queue-call') === -1) {
+    return null;
+  }
+  const name = headers['P-Asserted-Identity'][0].raw.split("\"")[1];
+  if (name) {
+    return `${name} - `
+  }
+  return null;
+}
+
+export function normalizeSession(session) {
+  return {
+    ...normalizeSessionBase(session),
+    callQueueName: getCallQueueName({
+      direction: session.__rc_direction,
+      headers: session.request && session.request.headers,
+    }),
+  }
+}
