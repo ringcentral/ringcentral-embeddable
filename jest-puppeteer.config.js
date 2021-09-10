5c9fcbdefe6d69445f244e08e9bad4ba07a614c4
Embbnux Ji <embbnux@embbnux.com>
2021-09-10 17:55:57 +0800

    update puppeteer config

--- jest-puppeteer.config.js
+++ jest-puppeteer.config.js
@@ -5,12 +5,10 @@ module.exports = {
     launchTimeout: 100000
   },
   launch: {
+    product: 'chrome',
     dumpio: true,
     args: ['--use-fake-ui-for-media-stream', '--use-fake-device-for-media-stream'],
     headless: process.env.TEST_HEADLESS === 'true'
-    // TODO: widget will check visible before init app,
-    // so in headless mode, widget will keep loadding.
   },
-  browser: 'chromium',
   browserContext: 'incognito',
 };
