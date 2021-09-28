a2f90838a6cbe4f2efc4ea505eda6b087c0c4588
Embbnux Ji <embbnux@embbnux.com>
2021-09-28 11:39:40 +0800

    disable brand theme build

--- build_brand_themes.sh
+++ build_brand_themes.sh
@@ -1,15 +1,16 @@
 #!/usr/bin/env bash
 
-# TODO: compile multiple themes in Webpack after mini-css-extract-plugin support https://github.com/webpack-contrib/mini-css-extract-plugin/issues/427 
+echo "No need this now, we use css variables to change theme"
+# # TODO: compile multiple themes in Webpack after mini-css-extract-plugin support https://github.com/webpack-contrib/mini-css-extract-plugin/issues/427 
 
-BRAND=att DISABLE_MINIMIZE=1 yarn build
-cp release/att/app.css release/app.att.css
-rm -rf release/att
+# BRAND=att DISABLE_MINIMIZE=1 yarn build
+# cp release/att/app.css release/app.att.css
+# rm -rf release/att
 
-BRAND=bt DISABLE_MINIMIZE=1 yarn build
-cp release/bt/app.css release/app.bt.css
-rm -rf release/bt
+# BRAND=bt DISABLE_MINIMIZE=1 yarn build
+# cp release/bt/app.css release/app.bt.css
+# rm -rf release/bt
 
-BRAND=telus DISABLE_MINIMIZE=1 yarn build
-cp release/telus/app.css release/app.telus.css
-rm -rf release/telus
+# BRAND=telus DISABLE_MINIMIZE=1 yarn build
+# cp release/telus/app.css release/app.telus.css
+# rm -rf release/telus
