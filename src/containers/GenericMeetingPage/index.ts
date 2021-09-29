28c290067ccece5f4b9348110a0d8bcca6acc86f
Embbnux Ji <embbnux@embbnux.com>
2021-09-29 21:51:36 +0800

    remove unused code

--- src/containers/GenericMeetingPage/index.ts
+++
@@ -1,9 +0,0 @@
-import { connectModule } from '@ringcentral-integration/widgets/lib/phoneContext';
-
-import { GenericMeetingPanel } from '../../components/GenericMeetingPanel';
-
-const GenericMeetingPage = connectModule((phone) => phone.genericMeetingUI)(
-  GenericMeetingPanel,
-);
-
-export default GenericMeetingPage;
