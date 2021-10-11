5102dd3488a4705c3906f720b1ca11f1098a8273
Embbnux Ji <embbnux@embbnux.com>
2021-10-11 14:10:53 +0800

    fix: glip permission check issue

--- src/modules/GlipGroups/index.js
+++ src/modules/GlipGroups/index.js
@@ -1,12 +1,22 @@
 import GlipGroups from '@ringcentral-integration/commons/modules/GlipGroups';
 import { Module } from '@ringcentral-integration/commons/lib/di';
 
-const glipGroupRegExp = /glip\/groups$/;
-
 @Module({
-  deps: [],
+  deps: [
+    'AppFeatures',
+  ],
 })
 export default class NewGlipGroups extends GlipGroups {
+  constructor({ appFeatures, ...options }) {
+    super(options);
+    this._appFeatures = appFeatures;
+  }
+
+  // TODO: update permission check in widgets lib
+  get _hasPermission() {
+    return !!this._appFeatures.hasGlipPermission;
+  }
+
   // TODO: hack for 400 error (Company associated with RC account is not found)
   async _fetchFunction() {
     try {
