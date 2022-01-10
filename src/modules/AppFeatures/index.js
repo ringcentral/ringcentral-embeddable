fefe4eddffeed79e37412d7507da05fc27b51ebf
Embbnux Ji <embbnux@embbnux.com>
2022-01-10 17:24:00 +0800

    fix: do not request video configration without Meetings permission

--- src/modules/AppFeatures/index.js
+++ src/modules/AppFeatures/index.js
@@ -11,4 +11,18 @@ export default class NewAppFeatures extends AppFeatures {
   get ringtonePermission() {
     return !!this.config.RingtoneSettings;
   }
+
+  get hasMeetingsPermission() {
+    return (
+      super.hasMeetingsPermission &&
+      this.appScopes.indexOf('Meetings') > -1
+    )
+  }
+
+  get appScopes() {
+    return (
+      this._deps.auth.token &&
+      this._deps.auth.token.scope
+    ) || '';
+  }
 }
