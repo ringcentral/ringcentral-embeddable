4b01531fa9593f3821b8c434acc36b5f4e8007b0
Embbnux Ji <embbnux@embbnux.com>
2022-05-20 11:03:23 +0800

    fix: auth refresh issue at multiple tabs with sleep detected (#623)
    * fix: auth refresh issue at multiple tabs with sleep detected
    
    * rename lock name

---
+++ src/lib/lockRefresh.js
@@ -0,0 +1,22 @@
+export default function lockRefresh(sdk) {
+  if (!navigator.locks) {
+    return sdk;
+  }
+  const platform = sdk.platform();
+  platform._$$refresh = platform._refresh;
+  const refreshWithLock = () => {
+    return navigator.locks.request('token_refresh', {mode: 'exclusive'}, async lock => {
+      const isRefreshed = await platform._auth.accessTokenValid()
+      if (isRefreshed) {
+        const authData = await platform._auth.data();
+        return new Response(authData, {
+          status: 200,
+        });
+      };
+      const res = await platform._$$refresh();
+      return res;
+    });
+  };
+  platform._refresh = refreshWithLock;
+  return sdk;
+}
