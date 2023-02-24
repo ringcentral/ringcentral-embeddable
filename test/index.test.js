eb35eb5399741629730d7dd6e9962cfaf416b380
Embbnux Ji <embbnux@embbnux.com>
2023-02-24 10:28:24 +0800

    chore: remove debug code

--- test/index.test.js
+++ test/index.test.js
@@ -14,7 +14,6 @@ describe('Index page test', () => {
   it('should display "RingCentral Embeddable" text on page', async () => {
     const title = await page.$eval('h1', (el) => el.innerText);
     expect(title).toContain('RingCentral Embeddable');
-    await page.waitForTimeout(100000);
   });
 
   it('should get SignIn in widget iframe', async () => {
