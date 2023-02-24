1b0467399bbe36069e1f868d97c876e6e96e3355
Embbnux Ji <embbnux@embbnux.com>
2023-02-24 14:16:48 +0800

    chore: remove unused code

--- src/modules/GlipCompany/index.js
+++ src/modules/GlipCompany/index.js
@@ -10,10 +10,6 @@ export default class NewGlipCompany extends GlipCompany {
   constructor({ appFeatures, ...options }) {
     super(options);
     this._appFeatures = appFeatures;
-    this._fetchFunction = async () => {
-      const response = await this._client.service.platform().get('/team-messaging/v1/companies/~');
-      return response.json();
-    };
   }
 
   // TODO: update permission check in widgets lib
