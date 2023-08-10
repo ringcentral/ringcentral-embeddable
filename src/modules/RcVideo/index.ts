6de44b6477b9524235b994c7193e9f001c4f3ebb
Embbnux Ji <embbnux@embbnux.com>
2023-08-10 16:44:30 +0800

    feat: use bridge v2 api

--- src/modules/RcVideo/index.ts
+++ src/modules/RcVideo/index.ts
@@ -3,6 +3,81 @@ import { RcVideo as RcVideoBase } from '@ringcentral-integration/commons/modules
 
 import { action, state } from '@ringcentral-integration/core';
 
+const PLAY_TONES_ENUM = {
+  On: 0,
+  Off: 1,
+  ExitOnly: 2,
+  EnterOnly: 3,
+};
+
+const WAITING_ROOM_MODE_ENUM = {
+  Nobody: 0,
+  Everybody: 1,
+  GuestsOnly: 2,
+  OtherAccount: 3,
+};
+
+function formatV2Bridge(bridge) {
+  return {
+    id: bridge.id,
+    name: bridge.name,
+    participantCode: bridge.pins.pstn.participant,
+    hostCode: bridge.pins.pstn.host,
+    shortId: bridge.pins.web,
+    joinUri: bridge.discovery.web,
+    allowJoinBeforeHost: bridge.preferences.joinBeforeHost,
+    type: 0,
+    announceOnEnter: bridge.preferences.join.pstn.promptAnnouncement,
+    countOnEnter: bridge.preferences.join.pstn.promptParticipants,
+    e2ee: bridge.security.e2ee,
+    musicEnabled: bridge.preferences.musicOnHold,
+    enterExitTonesMode: PLAY_TONES_ENUM[bridge.preferences.playTones],
+    muteAudio: bridge.preferences.join.audioMuted,
+    muteVideo: bridge.preferences.join.videoMuted,
+    accountId: bridge.host.accountId,
+    extensionId: bridge.host.extensionId,
+    isMeetingSecret: bridge.security.passwordProtected,
+    allowScreenSharing: bridge.preferences.screenSharing,
+    isOnlyAuthUserJoin: bridge.security.noGuests,
+    isOnlyCoworkersJoin: bridge.security.sameAccount,
+    waitingRoomMode: WAITING_ROOM_MODE_ENUM[bridge.preferences.join.waitingRoomRequired],
+    recordingsMode: bridge.preferences.recordingsMode,
+    transcriptionsMode: bridge.preferences.transcriptionsMode,
+    phoneGroup: 1,
+    meetingPassword: bridge.security.password.plainText,
+    meetingPasswordPSTN: bridge.security.password.pstn,
+    meetingPasswordMasked: bridge.security.password.joinQuery,
+  };
+}
+
+function formatBridgeRequestBody(meeting) {
+  if (meeting.type === 1) {
+    return {
+      type: 'Instant',
+    };
+  }
+  return {
+    name: meeting.name,
+    type: 'Scheduled',
+    preferences: {
+      join: {
+        audioMuted: meeting.muteAudio,
+        videoMuted: meeting.muteVideo,
+        waitingRoomRequired: Object.keys(WAITING_ROOM_MODE_ENUM)[meeting.waitingRoomMode],
+      },
+      joinBeforeHost: meeting.allowJoinBeforeHost,
+      screenSharing: meeting.allowScreenSharing,
+    },
+    security: {
+      passwordProtected: meeting.isMeetingSecret,
+      password: meeting.meetingPassword,
+      noGuests: meeting.isOnlyAuthUserJoin,
+      sameAccount: meeting.isOnlyCoworkersJoin,
+      e2ee: meeting.e2ee,
+    },
+  };
+}
+
 @Module({
   name: 'NewRcVideo',
   deps: []
@@ -43,7 +118,7 @@ export class RcVideo extends RcVideoBase {
   }
 
   async createInstantMeeting() {
-    const meeting = await this.createMeeting({ type: 0 });
+    const meeting = await this.createMeeting({ type: 1 });
     return meeting;
   }
 
@@ -142,5 +217,62 @@ export class RcVideo extends RcVideoBase {
 
   removeThirdPartyProvider({ name }) {
     delete this._thirdPartyProviders[name];
+  }
+
+  // override for bridge v2 api
+  override async _initPersonalMeeting(
+    accountId: number = this.accountId,
+    extensionId: number = this.extensionId,
+  ) {
+    if (!this._enablePersonalMeeting) {
+      return;
+    }
+    try {
+      const meetingResult = await this._deps.client.service
+        .platform()
+        .get(`/rcvideo/v2/account/${accountId}/extension/${extensionId}/bridges/default`);
+      const meeting = await meetingResult.json();
+      this._savePersonalMeeting(formatV2Bridge(meeting));
+    } catch (errors) {
+      console.error('fetch personal meeting error:', errors);
+      this._resetPersonalMeeting();
+      this._errorHandle(errors);
+    }
+  }
+
+  // override for bridge v2 api
+  async _postBridges(meetingDetail) {
+    const accountId = meetingDetail.accountId || `~`;
+    const extensionId = meetingDetail.extensionId || `~`;
+    const response = await this._deps.client.service
+      .platform()
+      .post(
+        `/rcvideo/v2/account/${accountId}/extension/${extensionId}/bridges`,
+        formatBridgeRequestBody(meetingDetail)
+      );
+    const result = await response.json();
+    return formatV2Bridge(result);
+  }
+
+  protected async _patchBridges(meetingId: string, meetingDetail) {
+    const body = formatBridgeRequestBody(meetingDetail);
+    delete body.type;
+    const result = await this._deps.client.service.platform().send({
+      method: 'PATCH',
+      url: `/rcvideo/v2/bridges/${meetingId}`,
+      body,
+    });
+    const meeting = await result.json();
+    return formatV2Bridge(meeting);
+  }
+
+  async getMeeting(
+    meetingId: string,
+  ) {
+    const result = await this._deps.client.service
+      .platform()
+      .get(`/rcvideo/v2/bridges/pin/web/${meetingId}`);
+    const meeting = await result.json();
+    return formatV2Bridge(meeting);
   }
 }
