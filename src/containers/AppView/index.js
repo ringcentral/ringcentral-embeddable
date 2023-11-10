1c75942ba81b6b1366d944a72b61f10c4f31ab1b
Embbnux Ji <embbnux.ji@ringcentral.com>
2023-11-10 22:45:55 +0800

    feat: add demo only alert

--- src/containers/AppView/index.js
+++ src/containers/AppView/index.js
@@ -1,15 +1,40 @@
 import React from 'react';
+
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 
 import { withPhone } from '@ringcentral-integration/widgets/lib/phoneContext';
-import { EnvironmentPanel } from '../../components/EnvironmentPanel';
+import {
+  RcAlert,
+  RcLink,
+} from '@ringcentral/juno';
+import { styled } from '@ringcentral/juno/foundation';
 
+import { EnvironmentPanel } from '../../components/EnvironmentPanel';
 import styles from './styles.scss';
+
+const DemoOnlyWarning = styled(RcAlert)`
+  padding: 8px 16px!important;
+
+  .RcAlert-message {
+    font-size: 12px !important;
+    line-height: 1.3;
+  }
+`;
 
 function AppView(props) {
   return (
     <div className={styles.root}>
+      <DemoOnlyWarning
+        severity="warning"
+        action={
+          
+        }
+      >
+        This app is using demo only client ID, please refer
+        <RcLink href="/test" target='_blank'>here</RcLink>
+        for details.
+      </DemoOnlyWarning>
       {props.children}
       <EnvironmentPanel
         server={props.server}
