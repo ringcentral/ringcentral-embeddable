3c9741822165c55b65238cb5a247d00ba523bb0f
Embbnux Ji <embbnux.ji@ringcentral.com>
2024-02-22 15:00:36 +0800

    fix: app view style with demo warning

--- src/containers/AppView/index.js
+++ src/containers/AppView/index.js
@@ -1,35 +1,49 @@
 import React from 'react';
 
-import classnames from 'classnames';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-
+import { styled, palette2 } from '@ringcentral/juno/foundation';
 import { withPhone } from '@ringcentral-integration/widgets/lib/phoneContext';
 
 import { DemoOnlyBanner } from '../../components/DemoOnlyBanner';
 import { EnvironmentPanel } from '../../components/EnvironmentPanel';
-import styles from './styles.scss';
+import './styles.scss';
+
+const Root = styled.div`
+  position: relative;
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+  display: flex;
+  flex-direction: column;
+  background: ${palette2('neutral', 'b01')};
+`;
+
+const Content = styled.div`
+  flex: 1;
+  overflow: hidden;
+`;
 
 function AppView(props) {
   return (
-    <div
-      className={classnames(styles.root, props.showDemoWarning ? styles.demoWarning : null)}
-    >
+    <Root>
       <DemoOnlyBanner
         show={props.showDemoWarning}
         onClose={props.dismissDemoWarning}
       />
-      {props.children}
-      <EnvironmentPanel
-        server={props.server}
-        enabled={props.enabled}
-        clientId={props.clientId}
-        clientSecret={props.clientSecret}
-        onSetData={props.onSetData}
-        redirectUri={props.redirectUri}
-        recordingHost={''}
-      />
-    </div>
+      <Content>
+        {props.children}
+        <EnvironmentPanel
+          server={props.server}
+          enabled={props.enabled}
+          clientId={props.clientId}
+          clientSecret={props.clientSecret}
+          onSetData={props.onSetData}
+          redirectUri={props.redirectUri}
+          recordingHost={''}
+        />
+      </Content>
+    </Root>
   );
 }
 
