a53c45dcde4f6990ea93778c9a55fae1e280ed93
Embbnux Ji <embbnux.ji@ringcentral.com>
2024-02-28 21:43:57 +0800

    fix: duplicated sms message event

---
+++ src/lib/isDuplicated.ts
@@ -0,0 +1,21 @@
+const getStorageKey = (groupName: string, id) => `${groupName}-${id}`;
+
+function clearExpiredKeys(groupName: string, maxKeys = 20) {
+  const keys = Object.keys(localStorage);
+  const groupKeys = keys.filter((key) => key.startsWith(groupName));
+  if (groupKeys.length > maxKeys) {
+    const sortedKeys = groupKeys.sort((a, b) => {
+      return Number(localStorage.getItem(a)) - Number(localStorage.getItem(b));
+    });
+    localStorage.removeItem(sortedKeys[0]);
+  }
+}
+
+export function isDuplicated(groupName: string, id, maxKeys = 20) {
+  const key = getStorageKey(groupName, id);
+  if (localStorage.getItem(key)) {
+    return true;
+  }
+  localStorage.setItem(key, `${Date.now()}`);
+  clearExpiredKeys(groupName, maxKeys);
+}
