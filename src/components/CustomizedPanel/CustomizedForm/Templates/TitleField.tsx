aa7f215e4dc939f4455b36f580c4e7899e6bf623
Embbnux Ji <embbnux@embbnux.com>
2024-09-27 17:33:40 +0800

    feat: customized page enhancement (#890)
    * feat: support navigation list
    
    * feat: replace checkbox with switch
    
    * feat: support collapsible for object value

--- src/components/CustomizedPanel/CustomizedForm/Templates/TitleField.tsx
+++ src/components/CustomizedPanel/CustomizedForm/Templates/TitleField.tsx
@@ -1,20 +1,58 @@
 import React from 'react';
 import {
   RcBox as Box,
-  RcDivider as Divider,
   RcTypography as Typography,
+  RcIcon,
+  RcDivider as Divider,
+  styled,
+  css,
 } from '@ringcentral/juno';
+import { ArrowDown2, ArrowUp2 } from '@ringcentral/juno-icon';
 
 import { FormContextType, TitleFieldProps, RJSFSchema, StrictRJSFSchema } from '@rjsf/utils';
+
+const StyledTitle = styled(Typography)`
+  flex: 1;
+`;
+
+const StyledBox = styled(Box)<{ $clickable: boolean }>`
+  display: flex;
+  align-items: center;
+  flex-direction: row;
+  ${({ $clickable }) => $clickable && css`
+    cursor: pointer;
+  `};
+`;
+
+const StyledIcon = styled(RcIcon)`
+  margin: 4px 0;
+`;
 
 export default function TitleField<T = any, S extends StrictRJSFSchema = RJSFSchema, F extends FormContextType = any>({
   id,
   title,
+  uiSchema = {},
+  extended = false,
+  onClick = undefined
 }: TitleFieldProps<T, S, F>) {
+  const collapsible = uiSchema['ui:collapsible'] || false;
   return (
-    <Box id={id} mb={1} mt={1}>
-      <Typography variant="title1">{title}</Typography>
-      <Divider />
-    </Box>
+    <StyledBox
+      id={id} mb={1} mt={1}
+      $clickable={collapsible}
+      onClick={onClick}
+    >
+      <StyledTitle variant="subheading1">{title}</StyledTitle>
+      {
+        collapsible ? (
+          <StyledIcon
+            symbol={extended ? ArrowUp2 : ArrowDown2}
+          />
+        ) : null
+      }
+      {
+        collapsible ? null : (<Divider />)
+      }
+    </StyledBox>
   );
 }
