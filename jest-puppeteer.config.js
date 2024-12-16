59dc7f89d24145095111e232fcb567678964b64e
Embbnux Ji <embbnux@embbnux.com>
2024-12-16 13:19:07 +0800

    chore: use latest ubuntu in github action (#935)

--- jest-puppeteer.config.js
+++ jest-puppeteer.config.js
@@ -8,7 +8,7 @@ module.exports = {
     product: 'chrome',
     dumpio: true,
     ignoreHTTPSErrors: true,
-    args: ['--use-fake-ui-for-media-stream', '--use-fake-device-for-media-stream', '--disable-features=IsolateOrigins,site-per-process'],
+    args: ['--use-fake-ui-for-media-stream', '--use-fake-device-for-media-stream', '--disable-features=IsolateOrigins,site-per-process', '--no-sandbox'],
     headless: process.env.TEST_HEADLESS === 'true'
   },
   browserContext: 'incognito',
