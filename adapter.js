e028a4aa17ac1696a7e688c68b04b4e707d17807
embbnux <embbnux@embbnux.com>
2024-12-19 08:14:26 +0000

    Deploying to gh-pages from  @ 04a77694dfe92c5672b8eaa55465cc468bd58238 🚀

--- adapter.js
+++ adapter.js
@@ -56859,7 +56859,7 @@ class Adapter extends AdapterCore/* default */.A {
           console.log(data.calls);
           break;
         case 'rc-login-status-notify':
-          console.log('rc-login-status-notify:', data.loggedIn, data.loginNumber, data.contractedCountryCode);
+          console.log('rc-login-status-notify:', data.loggedIn, data.loginNumber, data.contractedCountryCode, data.admin, data.features);
           break;
         case 'rc-calling-settings-notify':
           console.log('rc-calling-settings-notify:', data.callWith, data.callingMode);
