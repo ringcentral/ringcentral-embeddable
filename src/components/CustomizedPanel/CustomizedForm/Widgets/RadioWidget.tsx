728601e8b91216f3dee8e65b2a0cfa43356a6196
Embbnux Ji <embbnux.ji@ringcentral.com>
2025-04-01 10:15:13 +0800

    feat: support color for radio widget

--- src/components/CustomizedPanel/CustomizedForm/Widgets/RadioWidget.tsx
+++ src/components/CustomizedPanel/CustomizedForm/Widgets/RadioWidget.tsx
@@ -4,6 +4,8 @@ import {
   RcFormLabel as FormLabel,
   RcRadio as Radio,
   RcRadioGroup as RadioGroup,
+  styled,
+  css,
 } from '@ringcentral/juno';
 
 import {
@@ -18,6 +20,26 @@ import {
   WidgetProps,
 } from '@rjsf/utils';
 
+const StyledRadioGroup = styled(RadioGroup)<{flexDirection: string}>`
+  flex-direction: ${({ flexDirection }) => flexDirection};
+`;
+
+const StyledRadio = styled(Radio)<{ iconColor: string }>`
+  .RadioButtonIcon-root {
+    .MuiSvgIcon-root {
+      ${
+        ({ iconColor }) => iconColor && css`
+          fill: ${iconColor};
+        `
+      }
+    }
+  }
+`;
+
+const StyledFormLabel = styled(FormLabel)`
+  font-size: 0.75rem;
+`;
+
 export default function RadioWidget<T = any, S extends StrictRJSFSchema = RJSFSchema, F extends FormContextType = any>({
   id,
   options,
@@ -30,6 +52,7 @@ export default function RadioWidget<T = any, S extends StrictRJSFSchema = RJSFSc
   onChange,
   onBlur,
   onFocus,
+  uiSchema,
 }: WidgetProps<T, S, F>) {
   const { enumOptions, enumDisabled, emptyValue } = options;
 
@@ -41,16 +64,17 @@ export default function RadioWidget<T = any, S extends StrictRJSFSchema = RJSFSc
 
   const row = options ? options.inline : false;
   const selectedIndex = enumOptionsIndexForValue<S>(value, enumOptions) ?? null;
-
+  const flexDirection = uiSchema['ui:itemDirection'] || 'column';
+  const showValueLabel = typeof uiSchema['ui:valueLabel'] === 'undefined' ? true : uiSchema['ui:valueLabel'];
   return (
     <>
       {labelValue(
-        <FormLabel required={required} htmlFor={id}>
+        <StyledFormLabel required={required} htmlFor={id}>
           {label || undefined}
-        </FormLabel>,
+        </StyledFormLabel>,
         hideLabel
       )}
-      <RadioGroup
+      <StyledRadioGroup
         id={id}
         name={id}
         value={selectedIndex}
@@ -59,14 +83,23 @@ export default function RadioWidget<T = any, S extends StrictRJSFSchema = RJSFSc
         onBlur={_onBlur}
         onFocus={_onFocus}
         aria-describedby={ariaDescribedByIds<T>(id)}
+        flexDirection={flexDirection}
       >
         {Array.isArray(enumOptions) &&
           enumOptions.map((option, index) => {
             const itemDisabled = Array.isArray(enumDisabled) && enumDisabled.indexOf(option.value) !== -1;
             const radio = (
               <FormControlLabel
-                control={<Radio name={id} id={optionId(id, index)} color='primary' />}
-                label={option.label}
+                control={
+                  <StyledRadio
+                    name={id}
+                    id={optionId(id, index)}
+                    color={option.schema?.color}
+                    iconColor={option.schema?.color}
+                    useRcTooltip
+                    title={option.label}
+                  />}
+                label={showValueLabel ? option.label : undefined}
                 value={String(index)}
                 key={index}
                 disabled={disabled || itemDisabled || readonly}
@@ -75,7 +108,7 @@ export default function RadioWidget<T = any, S extends StrictRJSFSchema = RJSFSc
 
             return radio;
           })}
-      </RadioGroup>
+      </StyledRadioGroup>
     </>
   );
 }
