fd011ba7a974673442260dd7a3b37fd90d0c9d35
Embbnux Ji <embbnux.ji@ringcentral.com>
2025-06-10 16:27:50 +0800

    mics: 3.x with side widget

--- src/app.js
+++ src/app.js
@@ -191,7 +191,7 @@ const phone = createPhone({
   isUsingDefaultClientId,
   enableSMSTemplate,
   enableSmartNote,
-  // enableSideWidget,
+  enableSideWidget: !!enableSideWidget,
   enableAudioInitPrompt,
   enableSharedMessages,
   enableLoadMoreCalls,
